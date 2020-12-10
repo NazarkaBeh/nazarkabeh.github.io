a059bb0e0dda396d3883791534bedf40a3cad33d
NazarkaBeh <74146856+NazarkaBeh@users.noreply.github.com>
2020-12-10 19:03:42 +0200

    Delete Coffee.js

--- Зав_1/Coffee.js
+++
@@ -1,8 +0,0 @@
-function coffee()
-    {
-    console.log("В кав'ярні Владислав, Назарій, Юлія, Михайло, Денис, Олександр, Роман, В'ячеслав")
-    var names = Array("Владислав", "Назарій", "Юлія", "Михайло", "Денис", "Олександр", "Роман", "В'ячеслав");
-    var name = names[Math.floor(Math.random()*names.length)];
-    console.log("За каву платитиме:");
-    return name
-    }
