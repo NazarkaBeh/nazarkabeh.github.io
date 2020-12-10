07c9956f457e10d017a93143956c2ef8b4bfb9c9
NazarkaBeh <74146856+NazarkaBeh@users.noreply.github.com>
2020-12-10 19:03:55 +0200

    Delete GeneratorPar.js

--- Зав_1/GeneratorPar.js
+++
@@ -1,5 +0,0 @@
-var FirstName = prompt("Введіть перше імя");
-var SecondName = prompt("Введіть друге імя");
-var Vidsotok = Math.random() * 100;
-Vidsotok = Math.floor(Vidsotok);
-alert("Сумісність імен " + FirstName  + " та "  + SecondName + ' є ' + Vidsotok + "%");
